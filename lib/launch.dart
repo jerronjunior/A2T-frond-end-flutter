71aeaf227d845a1bf846ff08b7d11c23544b3ffb
jerronjunior <jerronjunior01@gmail.com>
2024-11-14 19:18:46 +0530

    modified launch page

--- lib/launch.dart
+++ lib/launch.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+class LaunchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.deepOrange.shade400, Colors.grey.shade900],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'S L R',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Sign Language Recognition',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Bridge the Hearing Gap',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white70,
+              ),
+            ),
+            SizedBox(height: 40),
+            Image.asset(
+              'assets/hand_image.png', // Ensure this image is in your assets folder
+              width: 150,
+              height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
